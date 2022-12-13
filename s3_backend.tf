ba682786774dd3de8f96d7be6d7b457a2f19c343
Muhiuddin Dewan <mohidewan10@gmail.com>
2022-12-13 14:36:39 +0600

    added comment in s3_backend

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,3 +1,5 @@
+# first need to create bucket from aws console as bucket name
+# important link: https://www.golinuxcloud.com/configure-s3-bucket-as-terraform-backend/
 terraform {
   backend "s3"{
     bucket = "terraform-state-bucket-77"
