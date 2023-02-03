aa87fced0eb81eeeb8a2919ca8b2701c6cbda205
Muhiuddin-Dewan <75271234+Muhiuddin-Dewan@users.noreply.github.com>
2023-02-03 14:59:23 +0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2_test" {
   ami               = "ami-0b0dcb5067f052a63"
   instance_type     = "t2.micro"
-  dynamodb_table    = "terraform-state"
+  #dynamodb_table    = "terraform-state"
   tags = {
     "name" = "my_first_ec2"
   }
