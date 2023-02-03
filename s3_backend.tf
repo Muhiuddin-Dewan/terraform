33b84ccc04a15bd8909ac321f0c58b51bfb009bd
Muhiuddin-Dewan <75271234+Muhiuddin-Dewan@users.noreply.github.com>
2023-02-03 14:54:55 +0600

    Update s3_backend.tf

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,9 +1,9 @@
 # first need to create bucket from aws console as bucket name
 # important link: https://www.golinuxcloud.com/configure-s3-bucket-as-terraform-backend/
-terraform {
-  backend "s3"{
-    bucket = "terraform-state-bucket-77"
-    key = "key/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3"{
+#    bucket = "terraform-state-bucket-77"
+#   key = "key/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
